54b9c92b9a2737f8c47396103918d788bf72cfb5
Emily Zheng <mzheng9@u.rochester.edu>
2021-10-24 02:41:30 -0400

    todo: recycler view

--- app/src/main/java/com/mzheng9/kotlin_fleamarket/HomeFragment.kt
+++ app/src/main/java/com/mzheng9/kotlin_fleamarket/HomeFragment.kt
@@ -5,17 +5,28 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.mzheng9.kotlin_fleamarket.databinding.FragmentHomeBinding
 
 
 class HomeFragment : Fragment() {
-
+    private var binding: FragmentHomeBinding? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        setHasOptionsMenu(true) // what is this
+        val fragmentHomeBinding = FragmentHomeBinding.inflate(inflater, container, false)
+        binding = fragmentHomeBinding
+        binding?.apply {
+
+            loginBtn.setOnClickListener {
+                findNavController().navigate(R.id.action_homeFragment_to_loginFragment)
+            }
+
+        }
+        return fragmentHomeBinding.root
     }
 
 
